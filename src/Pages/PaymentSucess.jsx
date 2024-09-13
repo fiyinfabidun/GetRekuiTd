5548e686dd199795de2044ab2c57bdb85d5cf8e5
fiyinfabidun <fiyinfoluwafabidun@gmail.com>
2024-09-13 15:13:49 +0100

    nav to pay

--- src/Pages/PaymentSucess.jsx
+++ src/Pages/PaymentSucess.jsx
@@ -1,27 +1,11 @@
 import React from "react";
-// import Navbar from "../Components/Navbar";
+import Navbar from "../Components/Navbar";
 // import { motion } from "framer-motion";
 
 function PaymentSucess() {
   return (
     <>
-    {/* <Navbar/>
-    <div className="pay-text">
-
-    <motion.div
-      className="loader-wrapper"
-      animate={{ rotate: 360 }}  // Rotate 360 degrees
-      transition={{ 
-        repeat: Infinity, 
-        duration: 2, 
-        ease: "linear" 
-      }}  // Loop the animation
-    >
-      <div className="loader"></div>
-    </motion.div>
-
-    </div> */}
-    <p>sucess</p>
+   <Navbar/>
     </>
   )
 }
