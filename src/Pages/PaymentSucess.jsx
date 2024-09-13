ed14800ade450df7eda27ad488511fee1376c6ce
fiyinfabidun <fiyinfoluwafabidun@gmail.com>
2024-09-13 15:34:10 +0100

    ,,

--- src/Pages/PaymentSucess.jsx
+++ src/Pages/PaymentSucess.jsx
@@ -3,22 +3,6 @@ import Navbar from "../Components/Navbar";
 import { motion } from "framer-motion"; // Importing framer-motion
 
 function PaymentSuccess() {
-  // Loader CSS as a constant
-  const loaderStyles = {
-    loaderWrapper: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-    },
-    loader: {
-      width: "100px",
-      height: "50px",
-      borderTop: "8px solid #3498db",
-      borderRight: "8px solid transparent",
-      borderRadius: "100px 100px 0 0",
-    },
-  };
 
   return (
     <>
@@ -26,16 +10,9 @@ function PaymentSuccess() {
         <Navbar />
       </div>
       <div className="pay-grid">
-        <motion.div
-          className="loader"
-          style={loaderStyles.loader}
-          animate={{ rotate: 360 }} // Full rotation animation
-          transition={{
-            duration: 1,
-            repeat: Infinity, // Loop animation
-            ease: "linear",
-          }}
-        ></motion.div>
+       <div className="loader">
+
+       </div>
       </div>
     </>
   );
