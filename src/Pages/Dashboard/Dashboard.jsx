81d9379134faa35aba3f73d7b1b18ee79b2ec173
fiyinfabidun <fiyinfoluwafabidun@gmail.com>
2024-09-15 10:57:28 +0100

    dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import logo from '../../Images/image.png';
 import { Link } from 'react-router-dom';
 import { CiGrid41 } from "react-icons/ci";
+import { LuLocateFixed } from "react-icons/lu";
+import { FaBusinessTime } from "react-icons/fa";
+import { PiLaptopLight } from "react-icons/pi";
+import { GoPerson } from "react-icons/go";
+
 
 
 const Dashboard = () => {
   return (
     <>
+    <div className="dash">
     <div className="dashboard container">
         <aside>
             <div className="top">
@@ -16,14 +22,39 @@ const Dashboard = () => {
                 </div>
             </div>
             <div className="sidebar">
-             <h3>
-               <Link to='/overview'>
-               <span><CiGrid41 /></span>
-               Overview
-               </Link>
-             </h3>
+                <h3>
+                    <Link>
+                    <span><CiGrid41 /></span>
+                    Overview
+                    </Link>
+                </h3>
+                <h3>
+                    <Link>
+                    <span><LuLocateFixed/></span>
+                    Job Tracker
+                    </Link>
+                </h3>
+                <h3>
+                    <Link>
+                    <span><FaBusinessTime /></span>
+                    My schedule
+                    </Link>
+                </h3>
+                <h3>
+                    <Link>
+                    <span><PiLaptopLight /></span>
+                    TechLab
+                    </Link>
+                </h3>
+                <h3>
+                    <Link>
+                    <span><GoPerson /></span>
+                    My Profile
+                    </Link>
+                </h3>
             </div>
         </aside>    
+    </div>
     </div>
     </>
   )
